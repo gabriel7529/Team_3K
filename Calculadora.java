c9eba5e2b35c0b85206a6160f5b66e2b257f9b80
fernandocoylaA <fcoylaa@gmail.com>
2022-04-29 18:29:21 -0500

    funcion sub, desarrollada

--- Calculadora.java
+++ Calculadora.java
@@ -4,8 +4,11 @@ class Calculadora {
   public class int add(int a , int b){
 	  return a + b; 	       
   }
+ /*Segunda funcion sub*/
+  public class int sub(int a , int b){ 
+	return a-b;  
+  }
 
-  int sub(int a, int b){ return 0; }
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
   int mod(int a, int b){ return 0; }
