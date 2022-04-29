7fdaabff224b2dd3289f17ab0907694a71003f54
fernandocoylaA <fcoylaa@gmail.com>
2022-04-29 18:25:24 -0500

    arreglo de organizacion de funcion add

--- Calculadora.java
+++ Calculadora.java
@@ -1,14 +1,15 @@
 import java.util.*;
 class Calculadora {
-  int add(int a, int b){ return 0; }
+ /*Primera funcion add*/	
+  public class int add(int a , int b){
+	  return a + b; 	       
+  }
+
   int sub(int a, int b){ return 0; }
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
   int mod(int a, int b){ return 0; }
-	public class int add(int a , int b){
-	  return a + b; 	       
-	}
-	public class int sub(int a , int b)
+	
 }
 
 
