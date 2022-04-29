1e8232c7c6e159de3e541a892cf3ee598115d6c1
fernandocoylaA <fcoylaa@gmail.com>
2022-04-29 18:10:30 -0500

    primera funcion add

--- Calculadora.java
+++ Calculadora.java
@@ -5,6 +5,10 @@ class Calculadora {
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
   int mod(int a, int b){ return 0; }
-
+	public class int add(int a , int b){
+	  return a + b; 	       
+	}
+	public class int sub(int a , int b)
 }
+
 
