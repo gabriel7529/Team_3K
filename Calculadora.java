2cf6917e092390df5b750f3c7df3a7e7c0d19d57
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-04-29 22:44:44 +0000

    Se creo la plantilla del proyecto

---
+++ Calculadora.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Calculadora {
+  int add(int a, int b){ return 0; }
+  int sub(int a, int b){ return 0; }
+  int mul(int a, int b){ return 0; }
+  int div(int a, int b){ return 0; }
+  int mod(int a, int b){ return 0; }
+
+}
+
