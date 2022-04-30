0f02038f926f8b8d1c946d2c04c2d1f2d2d20e1b
Edson López Quispe <elopezqu@unsa.edu.pe>
2022-04-29 19:07:26 -0500

    Se completa la función mod, para hallar el módulo de dos numeros

--- Calculadora.java
+++ Calculadora.java
@@ -1,23 +1,25 @@
 import java.util.*;
 class Calculadora {
- /*Primera funcion add*/	
-  public class int add(int a , int b){
-	  return a + b; 	       
-  }
- /*Segunda funcion sub*/
-  public class int sub(int a , int b){ 
-	return a-b;  
-  }
-  /*Tercera función mul*/
-  public class int mul(int a, int b){
-    return a*b;
-  }
-  /* Cuarta función div*/
-  public int div(int a, int b){ 
-    return a/b; 
-  }
-  int mod(int a, int b){ return 0; }
-	
+	/*Primera funcion add*/	
+	public class int add(int a , int b){
+		return a + b; 	       
+	}
+	/*Segunda funcion sub*/
+	public class int sub(int a , int b){ 
+		return a-b;  
+	}
+	/*Tercera función mul*/
+	public class int mul(int a, int b){
+		return a*b;
+	}
+	/*Cuarta función div*/
+	public int div(int a, int b){ 
+		return a/b; 
+	}
+	/*Quinta función mod*/
+	public int mod(int a, int b){ 
+		return a%b; 
+	}
 }
 
 
